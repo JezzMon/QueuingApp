c2a1f51380bda395441eac7f11b183e8a9de8fa3
Jezz <92468492+JezzMon@users.noreply.github.com>
2024-09-13 00:27:06 +0800

    Add files via upload

---
+++ QueueApp/CashierWindowQueueForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueApp
+{
+    public partial class CashierWindowQueueForm : Form
+    {
+        private Timer timer;
+        public CashierWindowQueueForm()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        public void DisplayCashierQueue(IEnumerable<string> cashierList)
+        {
+            listCashierQueue.Items.Clear();
+
+            foreach(object obj in cashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (CashierClass.CashierQueue.Count > 0)
+            {
+                CashierClass.CashierQueue.Dequeue();
+                DisplayCashierQueue(CashierClass.CashierQueue);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DisplayCashierQueue (CashierClass.CashierQueue);
+        }
+    }
+}
---
+++ QueueApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new QueuingForm());
+            */
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Create instances of both forms
+            QueuingForm queuingForm = new QueuingForm();
+            CashierWindowQueueForm cashierWindowForm = new CashierWindowQueueForm();
+
+            // Show both forms
+            queuingForm.Show();
+            cashierWindowForm.Show();
+
+            // Run the application using the main form (could be any of the two)
+            Application.Run(queuingForm);
+        }
+    }
+}
